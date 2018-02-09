23384200c1ea00e9d0f99f0c1db84e9d6bdf1a04
songzy <songzy.thu@qq.com>
2018-02-09 15:22:41 +0800

    squash

---
+++ src/ensemble_average.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pandas as pd
+import os
+import code
+
+output_path = '../output/'
+label_cols = ['toxic', 'severe_toxic', 'obscene', 'threat', 'insult', 'identity_hate']
+
+cnt = 0
+p0 = None
+for f in os.listdir(output_path):
+    cnt += 1
+    p = pd.read_csv(output_path+f)
+    if p0 == None:
+        p0 = p.copy()
+    else:
+        p0[label_cols] += p[label_cols]
+p0[label_cols] = p0[label_cols] / cnt
+
+p0.to_csv('submission.7z', index=False, compression='gzip')
--- src/run.sh
+++
@@ -1,3 +0,0 @@
-#time python3 tfidf_lr.py
-time python3 lstm.py
-time python3 ensemble.py
